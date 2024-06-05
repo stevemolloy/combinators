664543b415f92033402888d5dd7c5eb5c6dae7a7
Stephen Molloy <sdmolloy@gmail.com>
2024-06-05 16:21:21 +0200

    Tidy up
    Tidy up

--- combinators.py
+++ combinators.py
@@ -8,30 +8,43 @@ def K(x):
     return lambda y: x
 
 def W(f):
-    return lambda x: f(x, x)
+    return lambda x:    f(x, x)
 
 def C(f):
     return lambda x, y: f(y, x)
 
 def B(f, g):
-    return lambda x: f(g(x))
+    return lambda x:    f(g(x))
 
 def B1(f, g):
     return lambda x, y: f(g(x, y))
 
 def S(f, g):
-    return lambda x: f(x, g(x))
+    return lambda x:    f(x, g(x))
 
 def D(f, g):
     return lambda x, y: f(x, g(y))
 
+def D2(f, g, h):
+    return lambda x, y: f(g(x), h(y))
+
 def psi(f, g):
     return lambda x, y: f(g(x), g(y))
 
-# Examples
-square = lambda x: x**2
-plus = lambda x, y: x + y
-sqr_root = lambda x: x**0.5
+def phi(f, g, h):
+    return lambda x:    f(g(x), h(x))
 
-hypot = B1(sqr_root, psi(plus, square))
+def phi1(f, g, h):
+    return lambda x, y: f(g(x, y), h(x, y))
+
+# Examples
+KI = K(I)
+
+square =     lambda x:    x**2
+plus =       lambda x, y: x + y
+difference = lambda x, y: x - y
+sqr_root =   lambda x:    x**0.5
+
+hypot = B1(sqr_root, psi(plus, square)) # takes two numbers as input
+span = phi(difference, max, min)   # takes a single list as input
 
