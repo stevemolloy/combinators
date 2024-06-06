74c3a74280913f6abd03fc85bddec94031872d0a
Stephen Molloy <sdmolloy@gmail.com>
2024-06-06 19:17:42 +0200

    Church numerals

--- python_combinators.py
+++ python_combinators.py
@@ -60,3 +60,23 @@ NOT = lambda x: x(F)(T)
 AND = lambda x: lambda y: x(y)(x)
 OR  = lambda x: lambda y: x(x)(y)
 
+# Numerals
+church2int = lambda n: n(lambda x: x+1)(0) # Converts the Church numeral to Python int
+
+SUCC = lambda n: lambda f: lambda x: f(n(f)(x))
+ADD = lambda n1: lambda n2: n1(SUCC)(n2)
+MULTIPLY = B
+
+ZERO  = F
+ONE   = lambda f: lambda x: f(x)
+TWO   = lambda f: lambda x: f(f(x))
+THREE = lambda f: lambda x: f(f(f(x)))
+FOUR = SUCC(THREE)
+FIVE = SUCC(FOUR)
+SIX = SUCC(SUCC(SUCC(THREE)))
+SEVEN = ADD(THREE)(FOUR)
+EIGHT = ADD(SUCC(TWO))(FIVE)
+NINE = SUCC(ADD(ONE)(SEVEN))
+TEN = ADD(MULTIPLY(TWO)(THREE))(FOUR)
+TWENTY_SEVEN = MULTIPLY(THREE)(NINE)
+
