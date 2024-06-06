b75e75563abc97a94fdfd188a20dc83723593901
Stephen Molloy <sdmolloy@gmail.com>
2024-06-06 19:22:42 +0200

    Convert everything to lambdas. No defs.

--- python_combinators.py
+++ python_combinators.py
@@ -1,44 +1,21 @@
 # My attempt att summarising what I have learnt from:
 #    - https://raw.githubusercontent.com/codereport/Content/main/Publications/Combinatory_Logic_and_Combinators_in_Array_Languages.pdf
 
-def I(x):
-    return x
-
-def K(x):
-    return lambda y: x
-
-def W(f):
-    return lambda x: f(x)(x)
-
-def C(f):
-    return lambda x: lambda y: f(y)(x)
-
-def B(f):
-    return lambda g: lambda x: f(g(x))
-
-def B1(f):
-    return lambda g: lambda x: lambda y: f(g(x)(y))
-
-def S(f):
-    return lambda g: lambda x: f(x)(g(x))
-
-def D(f):
-    return lambda g: lambda x: lambda y: f(x)(g(y))
-
-def D2(f):
-    return lambda g: lambda h: lambda x: lambda y: f(g(x))(h(y))
-
-def psi(f):
-    return lambda g: lambda x: lambda y: f(g(x))(g(y))
-
-def phi(f):
-    return lambda g: lambda h: lambda x: f(g(x))(h(x))
-
-def phi1(f):
-    return lambda g: lambda h: lambda x: lambda y: f(g(x)(y))(h(x)(y))
+I    = lambda x: x
+K    = lambda x: lambda y: x
+KI   = K(I)
+W    = lambda f: lambda x: f(x)(x)
+C    = lambda f: lambda x: lambda y: f(y)(x)
+B    = lambda f: lambda g: lambda x: f(g(x))
+B1   = lambda f: lambda g: lambda x: lambda y: f(g(x)(y))
+S    = lambda f: lambda g: lambda x: f(x)(g(x))
+D    = lambda f: lambda g: lambda x: lambda y: f(x)(g(y))
+D2   = lambda f: lambda g: lambda h: lambda x: lambda y: f(g(x))(h(y))
+psi  = lambda f: lambda g: lambda x: lambda y: f(g(x))(g(y))
+phi  = lambda f: lambda g: lambda h: lambda x: f(g(x))(h(x))
+phi1 = lambda f: lambda g: lambda h: lambda x: lambda y: f(g(x)(y))(h(x)(y))
 
 # Examples
-KI = K(I)
 
 times =    lambda x: lambda y: x*y
 plus =     lambda x: lambda y: x + y
