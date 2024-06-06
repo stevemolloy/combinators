4f1f84cdbf01c2679d4254dfb776de85a909283e
Stephen Molloy <sdmolloy@gmail.com>
2024-06-06 17:34:34 +0200

    Functions can only take one argument

--- python_combinators.py
+++ python_combinators.py
@@ -8,44 +8,44 @@ def K(x):
     return lambda y: x
 
 def W(f):
-    return lambda x:    f(x, x)
+    return lambda x: f(x)(x)
 
 def C(f):
-    return lambda x, y: f(y, x)
+    return lambda x: lambda y: f(y)(x)
 
 def B(f, g):
-    return lambda x:    f(g(x))
+    return lambda x: f(g(x))
 
 def B1(f, g):
-    return lambda x, y: f(g(x, y))
+    return lambda x: lambda y: f(g(x)(y))
 
 def S(f, g):
-    return lambda x:    f(x, g(x))
+    return lambda x: f(x)(g(x))
 
 def D(f, g):
-    return lambda x, y: f(x, g(y))
+    return lambda x: lambda y: f(x)(g(y))
 
 def D2(f, g, h):
-    return lambda x, y: f(g(x), h(y))
+    return lambda x: lambda y: f(g(x))(h(y))
 
 def psi(f, g):
-    return lambda x, y: f(g(x), g(y))
+    return lambda x: lambda y: f(g(x))(g(y))
 
 def phi(f, g, h):
-    return lambda x:    f(g(x), h(x))
+    return lambda x: f(g(x))(h(x))
 
 def phi1(f, g, h):
-    return lambda x, y: f(g(x, y), h(x, y))
+    return lambda x: lambda y: f(g(x)(y))(h(x)(y))
 
 # Examples
 KI = K(I)
 
-times =    lambda x, y: x * y
-plus =     lambda x, y: x + y
-minus =    lambda x, y: x - y
-sqr_root = lambda x:    x**0.5
-equals =   lambda x, y: x == y
-reverse =  lambda xs:   xs[::-1]
+times =    lambda x: lambda y: x*y
+plus =     lambda x: lambda y: x + y
+minus =    lambda x: lambda y: x - y
+sqr_root = lambda x:           x**0.5
+equals =   lambda x: lambda y: x == y
+reverse =  lambda xs:          xs[::-1]
 
 square = W(times)                       # BQN: Square ← ×˜
 hypot = B1(sqr_root, psi(plus, square)) # BQN: Hypot ← √∘(+○(×˜))
