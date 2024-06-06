e2599d845862c9fe9cbfd3b60e147f18028451c5
Stephen Molloy <sdmolloy@gmail.com>
2024-06-06 19:25:29 +0200

    Typo

--- python_combinators.py
+++ python_combinators.py
@@ -1,4 +1,4 @@
-# My attempt att summarising what I have learnt from:
+# My attempt at summarising what I have learnt from:
 #    - https://raw.githubusercontent.com/codereport/Content/main/Publications/Combinatory_Logic_and_Combinators_in_Array_Languages.pdf
 
 I    = lambda x: x
